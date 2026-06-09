a3a8a72306c19d0a1fff50ed5425bd935b123b4a
zhaoq0901 <zhaoq0901@gmail.com>
2026-06-08 19:08:15 -0700

    Threshold

--- opencv_process.py
+++ opencv_process.py
@@ -3,24 +3,28 @@ import os
 import numpy as np
 from skimage import morphology, measure
 
-img = cv2.imread('bacteria_img/SAMPLE_2.png')
-name = "SAMPLE_2"
+img = cv2.imread('bacteria_img/SAMPLE_12.png')
+name = "SAMPLE_12"
 
 # Save original
 cv2.imwrite(os.path.join('output', f"{name}_original.png"), img)
 
 # Preprocess
-img = cv2.resize(img, (500, 500))
+# img = cv2.resize(img, (500, 500))
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
+gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-# cv2.imshow("Grayscale Image",gray)
-# cv2.waitKey(0)
+cv2.imshow("Grayscale Image",gray)
+cv2.waitKey(0)
 
 # Detect Petri dish
+
 circles = cv2.HoughCircles(
-    gray, cv2.HOUGH_GRADIENT, 1.2, 200,
-    param1=50, param2=30, minRadius=200, maxRadius=450
+    gray,
+    cv2.HOUGH_GRADIENT,
+    dp = 1.2,
+    minDist = gray.shape[0]//2,
+    param1=50, param2=25, minRadius=100, maxRadius=gray.shape[0]//2
 )
 if circles is None:
     print(f"No dish found in {name}")
@@ -38,24 +42,28 @@ cv2.imshow("Masked Image",masked)
 cv2.waitKey(0)
 
 # Remove tiny specks
-# kernel = np.ones((3,3), np.uint8)
+blur = cv2.bilateralFilter(masked, 9, 50, 50)  # preserves edges
+kernel = np.ones((2,2), np.uint8)
+clean = cv2.morphologyEx(blur, cv2.MORPH_OPEN, kernel, iterations=1)
+
+# kernel = np.ones((2,2), np.uint8)
 # clean = cv2.morphologyEx(masked, cv2.MORPH_OPEN, kernel, iterations=2)
 
-# cv2.imshow("Cleaned Image",clean)
-# cv2.waitKey(0)
+cv2.imshow("Cleaned Image",clean)
+cv2.waitKey(0)
 
-# cv2.imwrite(os.path.join('output', f"{name}_cleaned.png"), clean)
+cv2.imwrite(os.path.join('output', f"{name}_cleaned.png"), clean)
 
 # --- Threshold colonies ---
 thresh = cv2.adaptiveThreshold(
-    masked, 255,
+    clean, 255,
     cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
     cv2.THRESH_BINARY_INV,
-    51, 5
+    31, 2
 )
 
 Thresholded = morphology.remove_small_objects(thresh.astype(bool), min_size=50)
-Thresholded = (Thresholded * 255).astype(np.uint8)
+Thresholded = (Thresholded*255).astype(np.uint8)
 
 cv2.imwrite(os.path.join('output', f"{name}_thresholded.png"), Thresholded)
 
